3c65655b62013840ff596404de8fb2a893c7a791
cenyang <cenyang@bytedance.com>
2020-04-16 14:56:59 +0800

    add functions about bound

--- type.go
+++ type.go
@@ -1,9 +1,11 @@
 package cybuf
 
+import "unicode"
+
 type CybufType int
 
 const (
-	CybufType_nil CybufType = iota
+	CybufType_Nil CybufType = iota
 	CybufType_Number
 	CybufType_Decimal
 	CybufType_String
@@ -11,6 +13,34 @@ const (
 	CybufType_Object
 )
 
-func GetCybufType(value []rune) CybufType {
+func IsAllDigit(data []rune) bool {
+	for _, c := range data {
+		if !unicode.IsDigit(c) {
+			return false
+		}
+	}
+	return true
+}
 
+func IsBoundChar(c rune) bool {
+	switch c {
+	case '{', '}', '[', ']', '"', '\'':
+		return true
+	}
+	return false
+}
+
+// c must be a bound character
+func BoundMap(c rune) rune {
+	switch c {
+	case '{':
+		return '}'
+	case '}':
+		return '{'
+	case '[':
+		return ']'
+	case ']':
+		return '['
+	}
+	return c
 }
