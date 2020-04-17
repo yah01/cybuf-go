01cd80b76b2789cd3a7bdca344496b1d94fbcaf4
cenyang <cenyang@bytedance.com>
2020-04-17 23:13:57 +0800

    fix bugs when umarshal array; add marshal

--- cybuf.go
+++ cybuf.go
@@ -1,8 +1,8 @@
 package cybuf
 
 import (
-	"io/ioutil"
 	"log"
+	"os"
 	"reflect"
 	"strconv"
 	"strings"
@@ -10,13 +10,68 @@ import (
 )
 
 var (
-	// debugLog *log.Logger
-	errorLog *log.Logger
+	debugLog *log.Logger
+	// errorLog *log.Logger
 )
 
 func init() {
-	// debugLog = log.New(ioutil.Discard, "Debug ", log.LstdFlags|log.Lshortfile)
-	errorLog = log.New(ioutil.Discard, "Error ", log.LstdFlags|log.Lshortfile)
+	debugLog = log.New(os.Stdout, "Debug ", log.LstdFlags|log.Lshortfile)
+	// errorLog = log.New(ioutil.Discard, "Error ", log.LstdFlags|log.Lshortfile)
+}
+
+func Marshal(v interface{}) ([]byte, error) {
+	cybufBytes := []byte("{\n")
+	bytes, err := marshal(v, 1)
+	if err != nil {
+		return nil, err
+	}
+
+	cybufBytes = append(cybufBytes, bytes...)
+	cybufBytes = append(cybufBytes, '}')
+	return cybufBytes, nil
+}
+
+func marshal(v interface{}, tabCount int) ([]byte, error) {
+	var (
+		cybufBytes []byte
+	)
+
+	rv := v.(map[string]interface{})
+
+	tabsStr := ""
+	for i := 0; i < tabCount; i++ {
+		tabsStr += "\t"
+	}
+	tabsBytes := []byte(tabsStr)
+
+	for key, value := range rv {
+		var valueBytes []byte
+		cybufBytes = append(cybufBytes, tabsBytes...)
+		cybufBytes = append(cybufBytes, []byte(key)...)
+		cybufBytes = append(cybufBytes, ':', ' ')
+
+		realValue := reflect.ValueOf(value)
+		valueType := GetValueType(value)
+		switch valueType {
+		case CybufType_Nil:
+			valueBytes = []byte("nil")
+		case CybufType_Number:
+			valueBytes = strconv.AppendInt(valueBytes, realValue.Int(), 10)
+		case CybufType_Decimal:
+			valueBytes = strconv.AppendFloat(valueBytes, realValue.Float(), 'f', -1, 64)
+			//case CybufType_String:
+			//	switch realValue.Kind() {
+			//	case reflect.String:
+			//	case reflect.Slice
+			//	}
+			//case CybufType_Array:
+		}
+
+		cybufBytes = append(cybufBytes, valueBytes...)
+		cybufBytes = append(cybufBytes, '\n')
+	}
+
+	return cybufBytes, nil
 }
 
 func Unmarshal(data []byte, v interface{}) error {
@@ -27,7 +82,6 @@ func Unmarshal(data []byte, v interface{}) error {
 		}
 	}
 
-	//realData := []rune(string(data))
 	return unmarshal(data, v)
 }
 
@@ -39,8 +93,6 @@ func unmarshal(data []byte, v interface{}) error {
 		valueStr  string
 		valueType CybufType
 		err       error
-
-		//waitSub = sync.WaitGroup{}
 	)
 
 	data = []byte(strings.TrimSpace(string(data)))
@@ -55,7 +107,7 @@ func unmarshal(data []byte, v interface{}) error {
 
 		key, value, valueType, i, err = nextKeyValuePair(data, i)
 		if err != nil {
-			errorLog.Println(err)
+			// errorLog.Println(err)
 			return err
 		}
 
@@ -72,7 +124,7 @@ func unmarshal(data []byte, v interface{}) error {
 		}
 		valueStr = string(value)
 
-		//// debugLog.Println("value: "+string(value)+", valueType:", valueType)
+		// debugLog.Println("value: "+string(value)+", valueType:", valueType)
 		switch valueType {
 		case CybufType_Nil:
 			(*rv)[keyStr] = nil
@@ -87,7 +139,7 @@ func unmarshal(data []byte, v interface{}) error {
 			//waitSub.Add(1)
 			err := unmarshalArray(value, &array)
 			if err != nil {
-				errorLog.Println(err)
+				// errorLog.Println(err)
 				return err
 			}
 			(*rv)[keyStr] = array
@@ -96,14 +148,14 @@ func unmarshal(data []byte, v interface{}) error {
 			var object = make(map[string]interface{})
 			err := unmarshal(value, &object)
 			if err != nil {
-				errorLog.Println(err)
+				// errorLog.Println(err)
 				return err
 			}
 			// debugLog.Println(object)
 			(*rv)[keyStr] = object
 		}
 
-		// debugLog.Println("parsed:", keyStr, valueStr)
+		debugLog.Println("parsed:", keyStr, valueStr)
 	}
 
 	return nil
@@ -118,9 +170,11 @@ func unmarshalArray(data []byte, v *[]interface{}) error {
 	)
 
 	data = []byte(strings.TrimSpace(string(data)))
+	debugLog.Println(string(data))
 	if data[0] == '[' && data[len(data)-1] == ']' {
 		data = data[1 : len(data)-1]
 	}
+	debugLog.Println(string(data))
 
 	// debugLog.Println("unmarshal array data:", string(data))
 
@@ -128,10 +182,13 @@ func unmarshalArray(data []byte, v *[]interface{}) error {
 		value, valueType, i = nextValue(data, i)
 
 		if value == nil {
+			if i == len(data) {
+				break
+			}
 			return &ParseError{
 				Stage: ParseStage_Value,
 				Index: i,
-				Char:  rune(data[i]),
+				//Char:  rune(data[i]),
 			}
 		}
 		valueStr = string(value)
@@ -152,7 +209,7 @@ func unmarshalArray(data []byte, v *[]interface{}) error {
 			array := make([]interface{}, 0)
 			err := unmarshalArray(value, &array)
 			if err != nil {
-				errorLog.Println(err)
+				// errorLog.Println(err)
 				return err
 			}
 			realValue = array
@@ -161,7 +218,7 @@ func unmarshalArray(data []byte, v *[]interface{}) error {
 			var object = make(map[string]interface{})
 			err := unmarshal(value, &object)
 			if err != nil {
-				errorLog.Println(err)
+				// errorLog.Println(err)
 				return err
 			}
 			// debugLog.Println(object)
