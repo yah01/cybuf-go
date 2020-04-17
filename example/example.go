b364eea1c70a7f533e5a030dfde629d714177c1d
cenyang <cenyang@bytedance.com>
2020-04-17 23:14:13 +0800

    Update example.go

--- example/example.go
+++ example/example.go
@@ -12,26 +12,36 @@ func main() {
 	Age: 1
 	Weight: 100.2
 	School: {
-	  Name: "Wuhan University"
-	  Age: 120
+		Name: "Wuhan University"
+		Age: 120
 	}
-	Friends: [{
-		Name: "Zerone"
-		Phone: 01010101
-	}
-	{
-		Name: "Acm"
-		Phone: 2333
-	}]
+	Friends: [
+		{
+			Name: "Zerone"
+			Phone: 01010101
+		}
+		{
+			Name: "Acm"
+			Phone: 2333
+		}
+	]
 }
 `)
+	unmarshalMap := map[string]interface{}{}
 
-	testMap := map[string]interface{}{}
-
-	err := cybuf.Unmarshal(cybufBytes, &testMap)
+	err := cybuf.Unmarshal(cybufBytes, &unmarshalMap)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Printf("%+v", testMap)
+	fmt.Printf("%+v\n", unmarshalMap)
+
+	marshalMap := map[string]interface{}{
+		"First":  1,
+		"Second": 2,
+		"Half":   0.5,
+	}
+
+	cybufBytes, err = cybuf.Marshal(marshalMap)
+	fmt.Println(string(cybufBytes))
 }
