c552d3d2e59ed9927862fa97e664da9d9c3bd763
cenyang <cenyang@bytedance.com>
2020-04-23 23:22:21 +0800

    Make custom sep private; Provide a function SetMarshalSep() to set custom sep

--- cybuf.go
+++ cybuf.go
@@ -3,10 +3,16 @@ package cybuf
 var (
 	//debugLog *log.Logger
 	// errorLog *log.Logger
-	MarshalSep byte = ' '
+	marshalSep byte = ' '
 )
 
 func init() {
 	//debugLog = log.New(os.Stdout, "Debug ", log.LstdFlags|log.Lshortfile)
 	// errorLog = log.New(ioutil.Discard, "Error ", log.LstdFlags|log.Lshortfile)
+}
+
+func SetMarshalSep(sep byte) {
+	if sep != '\n' && sep != '\t' && sep != ' ' {
+		marshalSep = sep
+	}
 }
