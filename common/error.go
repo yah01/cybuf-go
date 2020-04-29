4ed029146d6703b012631dd8d382ab279eab7f4e
cenyang <cenyang@bytedance.com>
2020-04-29 15:46:52 +0800

    Add new error constants

--- common/error.go
+++ common/error.go
@@ -13,9 +13,17 @@ type (
 )
 
 const (
-	ParseStage_Key   MarshalStage = "key"
-	ParseStage_Colon MarshalStage = "colon"
-	ParseStage_Value MarshalStage = "value"
+	MarshalStage_Key   MarshalStage = "key"
+	MarshalStage_Colon MarshalStage = "colon"
+	MarshalStage_Value MarshalStage = "value"
+
+	UnmarshalStage_Key   UnmarshalStage = "key"
+	UnmarshalStage_Colon UnmarshalStage = "colon"
+	UnmarshalStage_Value UnmarshalStage = "value"
+
+	UnmarshalInfo_NoKey   UnmarshalInfo = "can't parse key"
+	UnmarshalInfo_NoColon UnmarshalInfo = "can't parse colon"
+	UnmarshalInfo_NoValue UnmarshalInfo = "can't parse value"
 )
 
 type MarshalError struct {
@@ -43,4 +51,17 @@ type UnmarshalError struct {
 	Type     CyBufType
 	Stage    UnmarshalStage
 	Info     UnmarshalInfo
+}
+
+func NewUnmarshalError(position int, typ CyBufType, stage UnmarshalStage, info UnmarshalInfo) *UnmarshalError {
+	return &UnmarshalError{
+		Position: position,
+		Type:     typ,
+		Stage:    stage,
+		Info:     info,
+	}
+}
+
+func (e *UnmarshalError) Error() string {
+	return fmt.Sprintf("cybuf: Unmarshal error when parsing %s at %d with type %+v: %s", e.Stage, e.Position, e.Type, e.Info)
 }
